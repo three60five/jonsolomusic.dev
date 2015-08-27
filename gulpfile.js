0fdf0489a8beeeb63bb94d14b787f605a0c3027e
Dave Allen <dave@three60five.com>
2015-08-27 14:50:12 -0700

    Added .gitignore file

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+      gutil = require('gulp-util');
+    // watch = require('gulp-watch'),
+    // livereload = require('gulp-livereload');
+
+
+gulp.task('default', function() {
+  return gutil.log('Gulp is running!')
+});
