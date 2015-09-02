cec452d14efea5be15bc781df09e9b23842ae7fd
Dave Allen <dave@three60five.com>
2015-09-02 15:39:42 -0700

    created project structure and updated build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,60 @@
-var gulp = require('gulp'),
-      gutil = require('gulp-util');
-    // watch = require('gulp-watch'),
-    // livereload = require('gulp-livereload');
+//Include gulp
+var gulp = require('gulp');
+
+//Include plugins
+var rename = require('gulp-rename');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var browserSync = require('browser-sync');
+var runSequence = require('run-sequence');
+var prefix = require('gulp-autoprefixer');
+
+// Start browserSync server
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: 'app'
+    }
+  })
+})
+
+// Compile SASS and reload browser
+gulp.task('sass', function() {
+  return gulp.src('app/sass/**/*.scss') // Gets all files ending with .scss in app/sass and children dirs
+    .pipe(sass()) // Passes it through a gulp-sass
+    .pipe(prefix({ cascade: true })) //autoprefix
+    .pipe(rename('styles.css')) //rename file
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(minifyCSS()) // minify css
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('dist/css')) // Outputs it in the css folder
+    .pipe(browserSync.reload({ // Reloading with Browser Sync
+      stream: true
+    }));
+})
+
+gulp.task('flickity-js', function() {
+  return gulp.src('node_modules/flickity/dist/flickity.pkgd.min.js')
+    .pipe(gulp.dest('app/js')) // Outputs it in the css folder
+    .pipe(gulp.dest('dist/js')) // Outputs it in the css folder
+})
+
+gulp.task('flickity-css', function() {
+  return gulp.src('node_modules/flickity/dist/flickity.min.css')
+    .pipe(gulp.dest('app/css')) // Outputs it in the css folder
+    .pipe(gulp.dest('dist/css')) // Outputs it in the css folder
+})
+
+// Watchers
+gulp.task('watch', function() {
+  gulp.watch('app/sass/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/js/**/*.js', browserSync.reload); 
+})
 
 
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
-});
+gulp.task('default', function(callback) {
+  runSequence(['sass', 'browserSync', 'watch'],
+    callback
+  )
+})
